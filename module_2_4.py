1adcb3e5db86036d19cd68ab88d20f203d7765d3
Raoof13 <kakmogu23@gmail.com>
2024-09-23 22:20:35 +0300

    23.09
    Signed-off-by: Raoof13 <kakmogu23@gmail.com>

--- module_2_4.py
+++ module_2_4.py
@@ -6,10 +6,12 @@ for i in numbers:
     if i == 1:
         continue
     is_prime = True
+
     for j in range(2, i):
         if i % j == 0:
+            is_prime = False
             break
-        is_prime = False
+
     if is_prime:
         primes.append(i)
     else:
