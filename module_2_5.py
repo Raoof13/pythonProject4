8b5f033eecd3cffca96fa30cbe6e5dab900ba14e
Raoof13 <kakmogu23@gmail.com>
2024-09-24 15:11:08 +0300

    23.09
    Signed-off-by: Raoof13 <kakmogu23@gmail.com>

--- module_2_5.py
+++ module_2_5.py
@@ -7,5 +7,9 @@ def get_matrix(n, m, value):
         for j in range(m):
             string_.append(value)
     return matrix
-result = get_matrix(2,2,10)
-print(result)
+result1 = get_matrix(2,2,10)
+result2 = get_matrix(3,5,42)
+result3 = get_matrix(4,2,13)
+print(result1)
+print(result2)
+print(result3)
