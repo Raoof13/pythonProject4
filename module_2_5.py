cd4bfdeffdda9814a36835d129f67f4d4f8c3c88
Raoof13 <kakmogu23@gmail.com>
2024-09-26 16:18:49 +0300

    23.09
    Signed-off-by: Raoof13 <kakmogu23@gmail.com>

--- module_2_5.py
+++ module_2_5.py
@@ -3,13 +3,13 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         string_ = []
-        if m <= 0 or value <= 0:
+        if m <= 0:
             continue
         matrix.append(string_)
         for j in range(m):
             string_.append(value)
     return matrix
-result1 = get_matrix(0,2,7)
+result1 = get_matrix(0,3,7)
 result2 = get_matrix(2,0,42)
 result3 = get_matrix(4,2,0)
 print(result1)
