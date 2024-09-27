41aa73f31946aaf8617c09c487525678a04df0c3
Raoof13 <kakmogu23@gmail.com>
2024-09-27 13:20:23 +0300

    23.09
    Signed-off-by: Raoof13 <kakmogu23@gmail.com>

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,14 @@
 
-def adventures(x, y):
-    list1 = []
-    num = int()
-    for x in range(len(list)):
-        list2 = []
-        for y in range(x+1, 21):
-            if x + y == num:
+def adventures(x):
+    stone1 = ''
 
+    for i in range(1, x):
+        for j in range(i + 1, x + 1):
+            if x % (i + j)== 0:
+                stone1 += str(i) + str(j)
+    return f'Ваш шифр: {stone1}'
 
+x = int(input("Число камня первого поля: "))
+
+ciphr = adventures(x)
+print(ciphr)
